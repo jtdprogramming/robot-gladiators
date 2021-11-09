54fc66042df57dc8bdc2d6b11c6ffa9ef5ca534d
James D <jtd.programming@gmail.com>
2021-11-09 18:32:20 -0500

    js updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,8 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
